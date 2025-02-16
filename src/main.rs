1893f0e694d5a492dba48db734f7d1c24f692d49
Gary Hilares <dev.garyhilares@gmail.com>
2025-02-16 01:04:45 -0800

    Added form to add transaction records

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use eframe::egui;
 use egui_extras::{Column, TableBuilder};
+use regex::Regex;
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
@@ -36,8 +37,52 @@ impl Record {
     }
 }
 
+struct FormInfo {
+    pub date: String,
+    pub description: String,
+    pub earnings: String,
+    pub spendings: String,
+}
+
+impl FormInfo {
+    pub fn new() -> FormInfo {
+        FormInfo {
+            date: "".to_owned(),
+            description: "".to_owned(),
+            earnings: "".to_owned(),
+            spendings: "".to_owned(),
+        }
+    }
+
+    pub fn try_to_parse_record(&self) -> Result<Record, &str> {
+        let re = Regex::new("([0-9]+)-([0-9]{2})-([0-9]{2})").unwrap();
+        if re.captures(&self.date).is_none() {
+            return Err("Invalid date found.");
+        }
+
+        let earnings = match self.earnings.parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => return Err("Invalid earnings amount found."),
+        };
+
+        let spendings = match self.spendings.parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => return Err("Invalid spendings amount found."),
+        };
+
+        return Ok(Record {
+            date: self.date.to_owned(),
+            description: self.description.to_owned(),
+            earnings_in_cents: earnings,
+            spendings_in_cents: spendings,
+        });
+    }
+}
+
 struct MyApp {
     records: Vec<Record>,
+    form_info: FormInfo,
+    error_message: Option<String>,
 }
 
 impl Default for MyApp {
@@ -49,6 +94,8 @@ impl Default for MyApp {
                 0u32,
                 8u32,
             )],
+            form_info: FormInfo::new(),
+            error_message: None,
         }
     }
 }
@@ -57,6 +104,35 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Transaction records");
+            ui.horizontal(|ui| {
+                ui.label("Date");
+                ui.text_edit_singleline(&mut self.form_info.date);
+            });
+            ui.horizontal(|ui| {
+                ui.label("Description");
+                ui.text_edit_singleline(&mut self.form_info.description);
+            });
+            ui.horizontal(|ui| {
+                ui.label("Earnings");
+                ui.text_edit_singleline(&mut self.form_info.earnings);
+            });
+            ui.horizontal(|ui| {
+                ui.label("Spendings");
+                ui.text_edit_singleline(&mut self.form_info.spendings);
+            });
+            if ui.button("Add").clicked() {
+                match self.form_info.try_to_parse_record() {
+                    Ok(result) => {
+                        self.form_info = FormInfo::new();
+                        self.records.push(result);
+                        self.error_message = None;
+                    }
+                    Err(error) => self.error_message = Some(error.to_owned()),
+                }
+            }
+            if let Some(error) = &self.error_message {
+                ui.label(error);
+            }
             TableBuilder::new(ui)
                 .column(Column::auto())
                 .column(Column::auto())
